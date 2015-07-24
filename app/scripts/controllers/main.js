0530db322ec48ad7a520ed25c132b4d9c68b7b78
mahaveer <mahaveer@kwench.in>
2015-07-24 22:58:08 +0530

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name planistoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the planistoApp
+ */
+angular.module('planistoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
